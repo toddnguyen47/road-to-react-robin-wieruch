38a028d071ab9744283396866d153954b2ec150b
Todd Nguyen <todd.nguyen47@gmail.com>
2020-09-28 01:07:31 -0700

    Finish React Component Instantiation

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -6,22 +6,70 @@ const welcome = {
   title: "React",
 };
 
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+/**
+ *
+ * @param {String} title
+ */
 function getTitle(title) {
   return "| " + title + " |";
+}
+
+function List() {
+  return (
+    <div>
+      {list.map(function (item) {
+        return (
+          <div key={item.objectID}>
+            <ul>
+              <a href={item.url}>{item.title}</a>
+              <li>Author: {item.author}</li>
+              <li>Number of Comments: {item.num_comments}</li>
+              <li>Points: {item.points}</li>
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 function App() {
   return (
     <div>
-      {/* <h1>Hello {title}</h1> */}
-      <h1>
-        {welcome.greeting} {welcome.title}
-      </h1>
+      <div>
+        {/* <h1>Hello {title}</h1> */}
+        <h1>
+          {welcome.greeting} {welcome.title}
+        </h1>
 
-      <h1>Hello {getTitle("React")}</h1>
+        <h1>Hello {getTitle("React")}</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" />
+      </div>
+
+      <hr />
+
+      <List />
     </div>
   );
 }
