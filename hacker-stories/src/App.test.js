9d4217cf40cd58634446d67705425df481953975
Todd Nguyen <todd.nguyen47@gmail.com>
2020-11-16 00:45:05 -0800

    Adding tests to check the input field and form submission
    for SearchForm

--- hacker-stories/src/App.test.js
+++ hacker-stories/src/App.test.js
@@ -101,4 +101,20 @@ describe("SearchForm", () => {
     // const value = component.root.findByType("input").props.value;
     expect(value).toEqual("React");
   });
+
+  it("changes the input field", () => {
+    const pseudoEvent = { target: "Redux" };
+    component.root.findByType("input").props.onChange(pseudoEvent);
+
+    expect(searchFormProps.onSearchInput).toHaveBeenCalledTimes(1);
+    expect(searchFormProps.onSearchInput).toHaveBeenCalledWith(pseudoEvent);
+  });
+
+  it("submits the form", () => {
+    const pseudoEvent = {};
+    component.root.findByType("form").props.onSubmit(pseudoEvent);
+
+    expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1);
+    expect(searchFormProps.onSearchSubmit).toHaveBeenCalledWith(pseudoEvent);
+  });
 });
